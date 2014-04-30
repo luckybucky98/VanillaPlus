7bc516d7c00c5e8ab2e4005d33f12b98724a3ad2
TheCricket <bcord@me.com>
2014-04-30 19:24:03 -0400

    Removed wood

--- common/vanilla+/src/main/java/com/cricket/vanillaplus/api/PreInitBlockCreating.java
+++ common/vanilla+/src/main/java/com/cricket/vanillaplus/api/PreInitBlockCreating.java
@@ -16,6 +16,5 @@ public class PreInitBlockCreating {
 	public static void blocks(){
 		Registry.BlockRockCrusherIdle = new BlockRockCrusher(1002,false).setUnlocalizedName("rockCrusherIdle").setHardness(3.5F).setCreativeTab(CreativeTab.VANILLAPLUS_TAB);
 		Registry.BlockRockCrusherActive = new BlockRockCrusher(1003,true).setUnlocalizedName("rockCrusherActive").setHardness(3.5F).setLightValue(0.9F);
-		Registry.BlockWood= new BlockWood(Registry.BlockWoodID).setUnlocalizedName("wood").setHardness(2.0F).setCreativeTab(CreativeTab.VANILLAPLUS_TAB);
 	}
 }
