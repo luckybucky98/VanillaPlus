f28858745274797e279cf327bfc2cb01791c6502
TheCricket <bcord@me.com>
2014-04-30 08:08:19 -0400

    Added Biome Compass

--- common/vanilla+/src/main/java/com/cricket/vanillaplus/api/PreInitBlockCreating.java
+++ common/vanilla+/src/main/java/com/cricket/vanillaplus/api/PreInitBlockCreating.java
@@ -3,6 +3,7 @@ package com.cricket.vanillaplus.api;
 import com.cricket.vanillaplus.blocks.BlockRockCrusher;
 import com.cricket.vanillaplus.creativetab.CreativeTab;
 import com.cricket.vanillaplus.items.ItemPebble;
+import com.cricket.vanillaplus.items.ItemBiomeCompass;
 
 public class PreInitBlockCreating {
 	public static void load(){
@@ -11,6 +12,7 @@ public class PreInitBlockCreating {
 	}
 	public static void items(){
 		Registry.ItemPebble = new ItemPebble(Registry.ItemPebbleid).setCreativeTab(CreativeTab.VANILLAPLUS_TAB);
+		Registry.ItemBiomeCompass = new ItemBiomeCompass(Registry.ItemBiomeCompassid).setCreativeTab(CreativeTab.VANILLAPLUS_TAB);
 	}
 	public static void blocks(){
 		Registry.BlockRockCrusherIdle = new BlockRockCrusher(1002,false).setUnlocalizedName("rockCrusherIdle").setHardness(3.5F).setCreativeTab(CreativeTab.VANILLAPLUS_TAB);
