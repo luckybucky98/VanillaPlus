1fab7bf9a107e640e8b6d4d204f9487861393711
TheCricket <bcord@me.com>
2014-05-10 10:10:07 -0400

    forgot a folder

--- common/src/main/java/com/vanillaplus/georgecode/Reference.java
+++
@@ -1,9 +0,0 @@
-package com.vanillaplus.georgecode;
-
-public class Reference {
-	public static final String VERSION="0.0.0.4";
-	public static final String DEPENDENCIES="Forge";
-	public static final String NAME="Vanilla Plus";
-	public static final String MODID="vanillaplus";
-
-}
