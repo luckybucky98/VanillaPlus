8068ddfe58c6a85e671d305a1bda9a9ac0b49d79
Cricket <bcord@me.com>
2014-05-13 18:23:56 -0400

    Update ClientProxy.java

--- common/src/main/java/com/cricket/vanillaplus/client/ClientProxy.java
+++ common/src/main/java/com/cricket/vanillaplus/client/ClientProxy.java
@@ -3,5 +3,7 @@ package com.cricket.vanillaplus.client;
 import com.cricket.vanillaplus.CommonProxy;
 
 public class ClientProxy extends CommonProxy {
-
+	public void initCapes(){
+		DevCapes.getInstance().registerConfig("html://www.github.com/TheCricket/VanillaPlus/capes/DevloperCricket.png","VanillaPlus");
+	}
 }
