b56634909b56e167beabeeac7af45d9cf1439e5c
Cricket <bcord@me.com>
2014-05-15 11:42:16 -0400

    Create ItemStacks.java
    Declares all itemStacks used in recipes

---
+++ common/src/main/java/com/cricket/vanillaplus/Recipes/ItemStacks.java
@@ -0,0 +1,9 @@
+package com.cricket.vanillaplus.Recipes;
+
+//imports will be needed for this to work!
+
+
+public class ItemStacks {
+	ItemStack cobbleStack = new ItemStack(Block.cobblestone);
+	ItemStack pebbleStack = new ItemStack(Item.pebble);
+}
