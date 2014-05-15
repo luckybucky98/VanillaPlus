be6d79707afb8b263782407a06721f4cbea6e4d1
Cricket <bcord@me.com>
2014-05-15 11:52:51 -0400

    Update Crafting.java

--- common/src/main/java/com/cricket/vanillaplus/Recipes/Crafting.java
+++ common/src/main/java/com/cricket/vanillaplus/Recipes/Crafting.java
@@ -1,5 +1,9 @@
 package com.cricket.vanillaplus.Recipes;
 
 public class Crafting {
-
+  public void loadCraftingRecipes(){
+    GameRegistry.addRecipe(new ItemStacks.pebbleStack, "xx","xx",'x',ItemStack.cobbleStack;
+    GameRegistry.addRecipe(new ItemStacks.rockCrusherStack, "xyx","xzx","xxx",'x',ItemStack.cobbleStack,'y',ItemStack.stonePickStack,'z',ItemStack.redstoneStack);
+    
+  }
 }
